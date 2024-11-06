3350142faf95e69d69aa567b6fe13cfe9070f321
larispohr <117369809+larispohr@users.noreply.github.com>
2024-11-06 11:55:16 -0300

    Update quantidadeUsuarios.js

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -9,8 +9,8 @@ async function quantidadeUsuariosPorRede() {
 
     const data = [
         {
-            x: nomedasmatérias, 
-            y: notadasmatérias, 
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
